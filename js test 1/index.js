127cfd1e903e5a83bb4df5d60f8a675758f3e739
privetitsmee <110345831+privetitsmee@users.noreply.github.com>
2023-02-27 18:49:18 +0200

    Add files via upload

---
+++ js test 1/index.js
@@ -0,0 +1,37 @@
+let button = document.getElementById("finish");
+
+button.addEventListener ("click" , checkTest);
+
+
+function checkTest(){
+    let result = 0
+
+    let a1 = document.getElementById("q1").value
+    if(a1 === "4"){
+        result++
+    }
+
+    let a2 = document.getElementById("q2").value
+    if(a2 === "20"){
+        result++
+    }
+
+    let a3 = document.getElementById("q3").value
+    if(a3 === "18"){
+        result++
+    }
+
+    let a4 = document.getElementById("q4").value
+    if(a4 === "4"){
+        result++
+    }
+
+    let a5 = document.getElementById("q5").value
+    if(a5 === "1"){
+        result++
+    }
+    
+
+    alert("Количество правильных ответов:" + result)
+}
+
